5a532cded803615aa0f8cf54f4c5524c3457cf52
Muamar Jasim <muamar78@gmail.com>
2025-09-04 14:21:29 +0000

    what is Icon of app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'Alatar QR Generator',
   description: 'Generate QR codes for direct Google Drive file downloads.',
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>큐</text></svg>',
+  },
 };
 
 export default function RootLayout({
