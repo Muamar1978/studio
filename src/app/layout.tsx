2739c96f6d95ac3d2c92bbfbb4a132f9597b798a
Muamar Jasim <muamar78@gmail.com>
2025-09-04 14:31:59 +0000

    you used 큐  as icon could change it somthing better

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   title: 'Alatar QR Generator',
   description: 'Generate QR codes for direct Google Drive file downloads.',
   icons: {
-    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>큐</text></svg>',
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 24 24%22 fill=%22none%22 stroke=%22black%22 stroke-width=%222%22 stroke-linecap=%22round%22 stroke-linejoin=%22round%22><rect width=%225%22 height=%225%22 x=%223%22 y=%223%22 rx=%221%22/><rect width=%225%22 height=%225%22 x=%2216%22 y=%223%22 rx=%221%22/><rect width=%225%22 height=%225%22 x=%223%22 y=%2216%22 rx=%221%22/><path d=%22M16 16h2v2h-2zM12 8h4M12 12h2M12 16h2m-4-4h2m-2-4h2m-2-4h2M8 12h2m-2-4h2m-4 8h2%22/></svg>',
   },
 };
 
